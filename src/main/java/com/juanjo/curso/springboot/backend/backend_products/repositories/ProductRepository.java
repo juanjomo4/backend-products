7788055a01094879ef14cb84c2d83925210be95b
Juanjo M. <60217633+juanjomo4@users.noreply.github.com>
2025-08-29 11:41:54 +0200

    Add CrossOrigin para conectar back con front

--- src/main/java/com/juanjo/curso/springboot/backend/backend_products/repositories/ProductRepository.java
+++ src/main/java/com/juanjo/curso/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -2,9 +2,11 @@ package com.juanjo.curso.springboot.backend.backend_products.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.juanjo.curso.springboot.backend.backend_products.entities.Product;
 
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
